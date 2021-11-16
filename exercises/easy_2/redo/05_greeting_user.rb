ec57087f474251a1cddf65240685ba69fae964ad
Blaise Clarke <blaiseclarke@protonmail.com>
2021-11-15 19:03:17 -0500

    Updating Easy 2 redo file

--- exercises/easy_2/redo/05_greeting_user.rb
+++ exercises/easy_2/redo/05_greeting_user.rb
@@ -57,7 +57,7 @@ print "What is your name? "
 name = gets.chomp
 
 if name[-1] == '!'
-  puts "HELLO #{name.chop.upcase}. WHY ARE WE SCREAMING?"
+  puts "HELLO #{name.chops.upcase}. WHY ARE WE SCREAMING?"
 else
   puts "Hello #{name}."
 end
