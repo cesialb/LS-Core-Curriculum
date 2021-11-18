40eedd82a9f7af4cdc923263edf47f6bb6b24a84
Blaise Clarke <blaiseclarke@protonmail.com>
2021-11-18 18:32:11 -0500

    Updating Easy 4 redo exercises

--- exercises/easy_4/redo/04_leap_years_pt_2.rb
+++ exercises/easy_4/redo/04_leap_years_pt_2.rb
@@ -71,6 +71,19 @@ def leap_year?(year)
   end
 end
 
+# better solution
+def leap_year?(year)
+  if year < 1752 && year % 4 == 0
+    true
+  elsif year % 400 == 0
+    true
+  elsif year % 100 == 0
+    false
+  else
+    year % 4 == 0
+  end
+end
+
 p leap_year?(2016) == true
 p leap_year?(2015) == false
 p leap_year?(2100) == false
