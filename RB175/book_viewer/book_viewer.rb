16cae0a7eeba7aaa45ccc2a78ffb34dcdc5ca050
cesialb <user.email>
2023-04-15 12:06:47 -0400

    Updating RB175 files

--- RB175/book_viewer/book_viewer.rb
+++ RB175/book_viewer/book_viewer.rb
@@ -2,21 +2,40 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
+
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
-
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
-  @title = "Chapter #{number}: #{chapter_name}"
   
+  redirect '/' unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
 
+get "/search" do
+  erb :search
+end
+
+not_found do
+  redirect "/"
+end
