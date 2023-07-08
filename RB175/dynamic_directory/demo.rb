562e046824232333da99e04a686a47d3a5e1e53a
cesialb <user.email>
2023-07-08 12:12:19 -0400

    Changing RB175 files

--- RB175/dynamic_directory/demo.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require 'tilt/erubis'
-
-get '/' do
-   @filenames = Dir["./public/*"].map {|file| File.basename(file) }.sort
-   @filenames.reverse! if params[:sort] == "desc"
-   erb :home
-end
